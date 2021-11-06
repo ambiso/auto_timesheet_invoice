6a52c8e1e94cf8b16425a4c55f2a45b967602d4f
ambiso <ambiso@invalid>
2021-11-06 13:50:55 +0100

    Generate invoice

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 #![feature(entry_insert)]
 
-use std::{ascii::AsciiExt, collections::{BTreeMap, HashMap}, error::Error, ops::Add, str::FromStr};
+use std::{collections::{BTreeMap, HashMap}, error::Error, str::FromStr};
 use num_rational::Rational64;
 use reqwest::Url;
 use serde::Deserialize;
 use tokio::{fs::File, io::AsyncReadExt};
 use serde_json::Value;
 use chrono::{DateTime, Datelike, Duration, Local, NaiveDate, TimeZone};
+extern crate chrono_tz;
 use chrono_tz::Tz;
+use kv::{Codec, Json, Store};
+
+mod accounting;
+mod timetracking;
 
 #[derive(Deserialize)]
 struct Toggl {
@@ -15,14 +20,26 @@ struct Toggl {
 }
 
 #[derive(Deserialize)]
+struct FreeFinance {
+    app_key: String,
+}
+
+#[derive(Deserialize)]
 struct Config {
     toggl: Toggl,
+    freefinance: FreeFinance,
     client: String,
     rate: i64
 }
 
 fn last_day_of_month(year: i32, month: u32) -> u32 {
-    NaiveDate::from_ymd_opt(year, month + 1, 1).unwrap_or(NaiveDate::from_ymd(year + 1, 1, 1)).pred().day()
+    NaiveDate::from_ymd_opt(year, month + 1, 1).unwrap_or_else(|| NaiveDate::from_ymd(year + 1, 1, 1)).pred().day()
+}
+
+fn ratio_to_string(r: Rational64) -> String {
+    let integer_part = r.to_integer();
+    let decimal_part = ((r - integer_part)*100).round().to_integer();
+    format!("{}.{:02}", integer_part, decimal_part)
 }
 
 #[tokio::main]
@@ -33,6 +50,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let config = toml::from_slice::<Config>(&buf)?;
     let client = reqwest::Client::new();
 
+    let url = accounting::freefinance::oauth_url(true, config.freefinance.app_key.as_str(), "", "");
+    println!("{}", url);
+    //return Ok(());
+
     let req = client.request(reqwest::Method::GET, "https://api.track.toggl.com/api/v8/me").basic_auth(&config.toggl.api_token, Some("api_token"));
     let response = req.send().await?;
     let response = response.json::<Value>().await?;
@@ -41,10 +62,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let local: DateTime<Local> = Local::now();
     let begin = tz.ymd(local.year(), local.month(), 1).and_hms(0, 0, 0);
+    let begin = begin - Duration::weeks(4*3);
     let begin = begin.format("%+").to_string();
     let end = tz.ymd(local.year(), local.month(), last_day_of_month(local.year(), local.month())).and_hms(23, 59, 59);
     let end = end.format("%+").to_string();
     println!("From {begin} to {end}", begin=begin, end=end);
+
+    let kv_cfg = kv::Config::new("./store/");
+
+    let store = Store::new(kv_cfg)?;
+
+    let billed_bucket = store.bucket::<kv::Integer, Json<bool>>(Some("billed"))?;
 
     let req = client.request(reqwest::Method::GET, Url::parse_with_params("https://api.track.toggl.com/api/v8/time_entries", &[
         ("start_date", begin.to_string()),
@@ -58,8 +86,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut clients = HashMap::new(); // Client ID -> Client Name 
     
     let mut summary = BTreeMap::new();
+
+    let mut to_bill = Vec::new();
+
     for entry in response {
-        let description = entry["description"].as_str().unwrap_or("Other");
+        let id = entry["id"].as_i64().expect("Expected time entry ID");
+
+        if billed_bucket.get(kv::Integer::from(id as u64)).expect("Could not read store").map(|x| x.into_inner()).unwrap_or(false) {
+            continue;
+        }
+
+        let description = entry["description"].as_str().unwrap_or("Other").trim();
         let duration = entry["duration"].as_i64().expect("Expected duration");
 
         let project_id = match entry["pid"].as_i64() {
@@ -109,16 +146,47 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         if client_name == config.client {
             *summary.entry(description.to_string()).or_insert(0) += duration;
+            to_bill.push(id);
         }
     }
 
     let mut sorted_by_duration: Vec<_> = summary.iter().map(|v| {
         let hours = Rational64::new(*v.1, 3600);
-        let price = hours * config.rate;
-        (hours, price, v.0)
+        let rounded_hours = (hours * 100).round() / 100;
+        (hours, rounded_hours, v.0)
     }).collect();
-    sorted_by_duration.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    sorted_by_duration.sort_by(|a, b| b.partial_cmp(a).unwrap());
 
-    dbg!(sorted_by_duration);
+    let deviation = sorted_by_duration.iter().map(|x| (x.1 - x.0) * config.rate).sum::<Rational64>();
+    dbg!(deviation);
+
+    billed_bucket.transaction(|tx| {
+        for id in &to_bill {
+            tx.set(kv::Integer::from(*id as u64), Json(true))?;
+        }
+        Ok(())
+    })?;
+    
+    let rate = Rational64::from(config.rate) / 100;
+    let mut total = Rational64::from(0);
+    let mut total_hours = Rational64::from(0);
+    for (i, x) in sorted_by_duration.iter().enumerate() {
+        let rounded_hours = x.1;
+        let description = x.2;
+        let price = rate * rounded_hours;
+        total += price;
+        total_hours += rounded_hours;
+        println!("{i} & {description} & {duration} & hrs & €{rate} & 0\\% & €{full_price} & €0.00 & €{full_price} \\\\",
+            i=i+1,
+            description=description.replace("&", "\\&"),
+            rate=ratio_to_string(rate),
+            duration=ratio_to_string(rounded_hours),
+            full_price=ratio_to_string(price),
+        );
+    }
+    println!(" &  &  &  & VAT & 0\\% & €{total} & €0.00 & €{total} \\\\\\hhline{{~~~~-----}} ", total=ratio_to_string(total));
+    println!(" &  &  &  & Total & EUR & €{total} & €0.00 & €{total} \\\\\\hhline{{~~~~=====}}", total=ratio_to_string(total));
+
+    println!("Total hours: {}", ratio_to_string(total_hours));
     Ok(())
 }
